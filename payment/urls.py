80e4b4603d201ba2a2a3099b29bb4342d0112faf
DeeStarks <danielonoja246@gmail.com>
2021-06-02 21:20:41 +0100

    route: included bank fetch and make payment endpoint in urls

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "payment"
 
 urlpatterns = [
-    path('banks', views.fetch_banks, name="fetch_banks")
+    path('banks', views.fetch_banks, name="fetch_banks"),
+    path('pay', views.make_payment, name="make_payment")
 ]
