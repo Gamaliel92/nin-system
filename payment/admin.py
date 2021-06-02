980c14ff5dbacbde48ffe287c5f98acf9fb6d811
DeeStarks <danielonoja246@gmail.com>
2021-06-02 19:19:23 +0100

    init: models for making payments, tokens after making payments, and api usage price

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.MakePayment)
+admin.site.register(models.ApiPrice)
+admin.site.register(models.PaymentToken)
--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Payment(models.Model):
-    pass
+class MakePayment(models.Model):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11, null=True)
+    bank = models.CharField(max_length=3, null=True)
+    account_number = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.email
+
+class ApiPrice(models.Model):
+    price = models.IntegerField(default=500)
+
+    def __str__(self):
+        return f"NGN {price}"
+
+class PaymentToken(models.Model):
+    payer = models.ForeignKey(MakePayment, on_delete=models.CASCADE)
+    secret = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.payer.email
