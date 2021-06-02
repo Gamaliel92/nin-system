b204ece13c87f57c51d9954401f5bd764a8e98b3
DeeStarks <danielonoja246@gmail.com>
2021-06-02 21:55:02 +0100

    alter: changed model name for nin linkage

--- nin_link/models.py
+++ nin_link/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class nin_links(models.Model):
+class LinkNin(models.Model):
     nin = models.CharField(max_length=50, null=True)
     phone_number = models.CharField(max_length=15, null=True)
     status = models.BooleanField(default=False)
